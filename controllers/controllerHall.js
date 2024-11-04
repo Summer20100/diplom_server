b328507e362ff8bd2b3f08d3f1c5a95f6a17edd5
Summer20100 <80061345+Summer20100@users.noreply.github.com>
2024-11-04 11:14:46 +0300

    Update controllerHall.js

--- controllers/controllerHall.js
+++ controllers/controllerHall.js
@@ -7,13 +7,13 @@ const addHall = async (req, res) => {
     pool.query(queriesHall.checkIsHallExist, [hall_title], (err, result) => {
       if (result.rows.length) {
         return res.status(400).json({
-          message: `${hall_title} already exists`,
+          error: `${hall_title} already exists`,
         });
       }
       pool.query(queriesHall.addHall, [hall_title], (err, result) =>{
         if (err) {
           return res.status(400).json({
-            message: "Hall not added",
+            error: "Hall not added",
           });
         }
       });
@@ -23,7 +23,7 @@ const addHall = async (req, res) => {
     })
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -45,12 +45,12 @@ const getHallById = async (req, res) => {
   try {
     const result = await pool.query(queriesHall.getHallById, [id]);
     if (result.rows.length === 0) {
-      return res.status(400).json({ message: "Hall doesn\'t exist" });
+      return res.status(400).json({ error: "Hall doesn\'t exist" });
     }
     return res.status(200).json(result.rows);
   } catch (err) {
     console.error("Error executing query", err);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -60,14 +60,14 @@ const removeHall = async (req, res) => {
     const result = await pool.query(queriesHall.getHallById, [id]);
     if (result.rows.length === 0) {
       return res.status(400).json({
-        message: "Hall is not exists",
+        error: "Hall is not exists",
       })
     }
     await pool.query(queriesHall.removeHall, [id]);
     return res.status(200).json({ hall: result.rows, message: `Hall deleted successfully` });
   } catch (err) {
     console.error("Error executing query", err);
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 
 }
